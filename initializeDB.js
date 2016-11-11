1d6f7acef86f6581200b796110720fa3c66cea15
Bradford <bwbonanno@wpi.edu>
2016-11-11 00:50:47 -0500

    Initial commit

---
+++ initializeDB.js
@@ -0,0 +1,28 @@
+var sqlite = require('sqlite3');
+var fs = require('fs');
+var db = new sqlite.Database('movies');
+
+var filestream = fs.createReadStream('initial_movies.txt');
+
+var data = '';
+filestream.on('data', function(c){
+	data = data + c;
+});
+
+filestream.on('end', function(){
+	data = data.split('\n');
+	console.log(data);
+	
+	db.serialize(function(){
+		db.run("CREATE TABLE movies (title varchar(100), rating int)");
+		data.forEach(function(d){
+			if(d){
+				db.run("INSERT INTO movies VALUES ('"+d+"', 5)");
+			}
+		});
+
+		db.close();
+	});
+});
+
+
--- server.js
+++ server.js
@@ -3,37 +3,75 @@ var http = require('http')
 	, qs   = require("querystring")
 	, url  = require('url')
 	, imdb = require('imdb-api')
+	, async = require('async')
 	, HashMap = require('hashmap')
 	, deasync = require('deasync')
+	, sqlite  = require('sqlite3')
 	, port = 8080
 
-// Add more movies! (For a technical challenge, use a file, or even an API!)
-var movies = ['Jaws', 'Jaws 2', 'Jaws 3', 'Doctor Strange', "Star Wars", "The Jungle Book", "Batman vs. Superman", "The Hunger Games",
-	"The Godfather", "Citizen Kane", "The Great Gatsby", "Breakfast Club", "Beetlejuice", "Lion King", "Hitchiker's Guide to the Galaxy", "Mulan", "Aladdin", "Tarzan", "Tangled",
-	"Frozen", "Lord of the Rings", "Harry Potter", "Star Trek", "Fight Club", "A Beautiful Mind", "Rain Man", "The Departed", "The Bee Movie", "The Big Lebowski", "Guardians of the Galaxy", "Snakes on a Plane", "Pulp Fiction", "The Truman Show", "A Christmas Carol", "Sharknado", "Little Miss Sunshine", "Legends of the Fall", "The Fountain", "500 Days of Summer", "Iron Man"]
-movies = movies.sort();
-movies = movies.map(function(movie){
-	return movie = movie + " ";	
+var db = new sqlite.Database('movies');
+
+var movies = [];
+
+db.each("SELECT * FROM movies", function(err, title){
+	movies.push(title.title);
+}, function(){
+	movies = movies.sort();
+	movies = movies.map(function(movie){
+		return movie = movie + " ";	
+	})
+	server.listen(process.env.PORT || port)
+	console.log('listening on 8080')
 });
 
+// Add more movies! (For a technical challenge, use a file, or even an API!)
+
+
+
+
 var server = http.createServer (function (req, res) {
+
 	var uri = url.parse(req.url)
 
+	var data = '';
+	var q;
+
+	req.on('data', function(c){
+		data = data + c;
+	});
+
+	req.on('end', function(){
+		handleURI(req,res,uri,data);
+	});
+
+})  
+
+
+var info;
+var hm = new HashMap();
+var index = new HashMap();
+function handleURI(req, res, uri, data){
 	switch( uri.pathname ) {
 			// Note the new case handling search
 		case '/search':
 			handleSearch(res, uri)
 			break
-			// Note we no longer have an index.html file, but we handle the cases since that's what the browser will request
 		case '/':
-			sendIndex(res)
+			if(data){ updateMovies(req, res, uri, data) }
+			else { sendIndex(res) };
 			break
 		case '/index.html':
-			sendIndex(res)
+			sendIndex(res);
 			break
 		case '/css/style.css':
 			sendFile(res, 'css/style.css', 'text/css')
 			break
+		case '/node_modules/bootstrap/dist/css/bootstrap.css':
+			sendFile(res, 'node_modules/bootstrap/dist/css/bootstrap.css', 'text/css')
+			break
+		case '/node_modules/bootstrap/dist/css/bootstrap-theme.css':	
+			sendFile(res,'node_modules/bootstrap/dist/css/bootstrap-theme.css', 'text/css')
+			break;	
 		case '/js/scripts.js':
 			sendFile(res, 'scripts.js', 'text/javascript')
 			break
@@ -43,11 +81,12 @@ var server = http.createServer (function (req, res) {
 		default:
 			res.end('404 not found')
 	}
+}
 
-})  
 
-server.listen(process.env.PORT || port)
-console.log('listening on 8080')
+
+// subroutines
+var html = '';
 var info;
 var hm = new HashMap();
 var index = new HashMap();
@@ -113,6 +152,15 @@ function imdbQuery(query_copy)
 				"<li class='info'>Rating: " + data.rated + "</li>" + 
 				"<li class='info'>Runtime: " + data.runtime + "</li>";
 		}
+		db.run("INSERT INTO movies VALUES ('" + query_copy + "', 5)", function(){
+			movies.push(query_copy);
+			movies = movies.sort();
+			movies = movies.map(function(movie){
+				return movie = movie + " ";	
+			});
+
+		});
+
 
 	});
 	while(info === undefined){
@@ -138,7 +186,7 @@ function handleSearch(res, uri) {
 		console.log(query_copy);
 		query = query.search.split(" ");
 		query = query.map(function(q){
-				return q = q + " ";
+			return q = q + " ";
 		});
 
 		var results = [];
@@ -171,9 +219,8 @@ function handleSearch(res, uri) {
 			});
 		results = firstSort(index, results, -1);
 		results = secondSort(hm, results, 1);
-		
-		count = results.length;
 
+		count = results.length;
 
 		// Write HTML Results Page //
 		var contentType = 'text/html';
@@ -181,6 +228,8 @@ function handleSearch(res, uri) {
 		html = html + "<head>";
 		// Insert CSS Sheet Here //
 		html = html + "<title>Results</title>";
+		html = html + "<link href='node_modules/bootstrap/dist/css/bootstrap.css' rel='stylesheet'>";
+		html = html + "<link href='node_modules/bootstrap/dist/css/bootstrap-theme.css' rel='stylesheet'>"
 		html = html + "<link rel='stylesheet' type='text/css' href='/css/style.css'/>";
 
 		html = html + "</head>";
@@ -189,14 +238,16 @@ function handleSearch(res, uri) {
 		html = html + "<h1> RESULTS </h1>";
 		if (count >= 5)
 		{
+			html = html + "<div class='links-div'>";
 			html = html + "<a href='/' class='links'>Search Again</a>";
+			html = html + "</div>";
 		}
 		html = html + "<div class='result-div'>";
 		html = html + "<ul class='col'>";
 
 		if(hm.get(results[0]) < query.length)
 		{
-			//imdbQuery(query_copy);
+			imdbQuery(query_copy);
 		}
 		if (count <= 0)
 		{
@@ -259,7 +310,9 @@ function completeHTML(res, contentType){
 	html = html + "</ul>";
 	html = html + "</div>";
 
+	html = html + "<div class='links-div'>";
 	html = html + "<a href='/' class='links'>Search Again</a>";
+	html = html + "</div>";
 
 	html = html + "</body>";
 	html = html + "</html>";
@@ -269,8 +322,23 @@ function completeHTML(res, contentType){
 	res.end(html, 'utf-8')
 }
 
+function updateMovies(req, res, uri, data){
+	q = qs.parse(data);
+	var title = q.title;
+
+	db.run("INSERT INTO movies VALUES ('" + title + "', 5)", function(){
+		movies.push(title);
+		ratingsMap.set(title, 5);
+		sendIndex(res);
+
+	});
+}
+
+
 // Note: consider this your "index.html" for this assignment
 function sendIndex(res) {
+
+
 	var contentType = 'text/html'
 		, html = ''
 
@@ -278,7 +346,10 @@ function sendIndex(res) {
 
 	html = html + '<head>'
 	// You could add a CSS and/or js call here...
+	html = html + "<link href='node_modules/bootstrap/dist/css/bootstrap.css' rel='stylesheet'>";
+	html = html + "<link href='node_modules/bootstrap/dist/css/bootstrap-theme.css' rel='stylesheet'>"
 	html = html + "<link rel='stylesheet' type='text/css' href='/css/style.css'/>";
+
 	html = html + "<title>Movie Search!</title>";
 	html = html + '</head>'
 
@@ -288,12 +359,12 @@ function sendIndex(res) {
 	html = html + "</div>"
 
 	// Here's where we build the form YOU HAVE STUFF TO CHANGE HERE
-	html = html + '<form action="search" method="search" class="search">'
-	html = html + '<input type="search" name="search" />'
-	html = html + '<button type="search">Search</button>'
+	html = html + '<form action="search" method="get" class="search">'
+	html = html + '<input type="String" name="search" />'
+	html = html + '<button type="submit">Search</button>'
 	html = html + '</form>'
 
-	html = html + '<ul class="col">'
+	html = html + '<div class="col">'
 	// Note: the next line is fairly complex. 
 	// You don't need to understand it to complete the assignment,
 	// but the `map` function and `join` functions are VERY useful for working
@@ -301,16 +372,46 @@ function sendIndex(res) {
 	// and read up on the functions it uses.
 	//
 	// For a challenge, try rewriting this function to take the filtered movies list as a parameter, to avoid changing to a page that lists only movies.
-	html = html + movies.map(function(d) { return '<li class="searchable"><a href="/search?search='+d+'">'+d+'</a></li>' }).join(' ')
-	html = html + '</ul>'
-	html = html + "<a href='/README.md' class='links'>README</a>";
 
-	html = html + '</body>'
-	html = html + '</html>'
+	console.log(movies);
 
-	res.writeHead(200, {'Content-type': contentType})
-	res.end(html, 'utf-8')
-	html = "";
+	async.map(movies, function(d, callback) {
+
+
+		db.get("SELECT * FROM movies WHERE title='"+d.substring(0,d.length-1)+"'", function(error, rating){
+
+			if(rating){
+
+				var info = '';
+				info = info + "<a href='/search?search="+d+"'>";
+				info = info + "<div class=panel panel-default'>";
+				info = info + "<div class='panel-heading'>";
+				info = info + "<h3 class='panel-title'>";
+				info = info + d;
+				info = info + "</h3>";
+				info = info + "</div>";
+				info = info + "<div class='panel-body'>";
+				info = info + "Rating: " + rating.rating; 
+				info = info + "</div>";
+				info = info + '</div>'
+				info = info + '</a>';
+				callback(null, info); 
+			}
+		})}, function(error, results){
+				results = results.join(' ');
+				html = html + results;
+				html = html + "</div>";
+				html = html + "<div class='links-div'>";
+				html = html + "<a href='/README.md' class='links'>README</a>";
+				html = html + "</div class='links-div>";
+
+				html = html + '</body>'
+				html = html + '</html>'
+
+				res.writeHead(200, {'Content-type': contentType})
+				res.end(html, 'utf-8')
+				html = "";
+			})
 }
 
 function sendFile(res, filename, contentType) {
